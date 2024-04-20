927d53f91675e67d90fc5d95829ce19d9b6972c2
Mathias Törnblom <mathias@tornbloms.net>
2024-04-20 10:50:23 +0200

    Initial Commit

---
+++ Event QR Analytics/Event_QR_AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_QR_AnalyticsApp.swift
+//  Event QR Analytics
+//
+//  Created by Mathias Törnblom on 2024-04-20.
+//
+
+import SwiftUI
+
+@main
+struct Event_QR_AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
